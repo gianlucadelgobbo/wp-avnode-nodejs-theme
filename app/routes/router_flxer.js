d8546e67e011e701d4841a1b9aa1f8ea9ca59d4f
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2019-06-02 23:36:06 +0200

    flxer redirect

--- app/routes/router_flxer.js
+++ app/routes/router_flxer.js
@@ -7,6 +7,19 @@ var robotsRoutes = require('./_common/robots');
 var metaRoutes = require('./_common/meta');
 
 module.exports = function(app) {
+  app.get('/performers', function(req, res) {console.log(req.url);res.redirect(301, 'https://avnode.net/performers/')});
+
+  app.get('/:performer/performances/:performance/', function(req, res) {res.redirect(301, 'https://avnode.net/performances/'+req.params.performance)});
+  app.get('/:performer/events/:event/', function(req, res) {res.redirect(301, 'https://avnode.net/events/'+req.params.event)});
+  app.get('/:performer/tvshows/:tvshow/', function(req, res) {res.redirect(301, 'https://avnode.net/videos/'+req.params.tvshow)});
+  app.get('/:performer/footage/:footage/', function(req, res) {res.redirect(301, 'https://avnode.net/footage/'+req.params.footage)});
+  app.get('/:performer/playlists/:playlist/', function(req, res) {res.redirect(301, 'https://avnode.net/playlists/'+req.params.playlist)});
+  app.get('/:performer/performances/:performance/', function(req, res) {res.redirect(301, 'https://avnode.net/performances/'+req.params.performance)});
+  app.get('/performers/*', function(req, res) {console.log(req.url);res.redirect(301, req.url.replace('/performers/','https://avnode.net/'))});
+  app.get('/tvshows', function(req, res) {res.redirect(301, 'https://avnode.net/videos/')});
+  app.get('/footage', function(req, res) {res.redirect(301, 'https://avnode.net/footage/')});
+  app.get('/playlists', function(req, res) {res.redirect(301, 'https://avnode.net/playlists/')});
+
   app.get('/', indexRoutes.get);
 
   app.get('/meta/', metaRoutes.get);
@@ -26,6 +39,7 @@ module.exports = function(app) {
   
   app.post('/(:page)/(:subpage)', pagesRoutes.post);
   app.post('/signup', signupRoutes.post);
+
 
   app.get('*', pagesRoutes.get404);
 };
