9b9a6771073dadcf591df9cc8baa044de530aae0
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2021-07-20 11:44:31 +0200

    lcf 2021

--- gulp/js/lcf/script.js
+++ gulp/js/lcf/script.js
@@ -1,15 +1,37 @@
 $(function() {
   $('#menu-main').on('hidden.bs.collapse', function () {
-      $( "#navbar-top-cnt" ).addClass( "position-fixed" );
-      $( "#navbar-top-cnt" ).removeClass( "position-absolute" );
-      console.log("hidden")
-    })
+    $( "#navbar-top-cnt" ).addClass( "position-fixed" );
+    $( "#navbar-top-cnt" ).removeClass( "position-absolute" );
+    console.log("hidden")
+  })
   $('#menu-main').on('show.bs.collapse', function () {
     $( "#navbar-top-cnt" ).addClass( "position-absolute" );
     $( "#navbar-top-cnt" ).removeClass( "position-fixed" );
     $(window).scrollTop(0);
   })
-/*   function bodyPadding() {
+  $('.filters .by-type a').on('click', function (event) {
+    event.preventDefault();
+    if ($(this).data("show")=="all") {
+      $('.sameheight').removeClass( "d-none" );
+      $('.itemListElement').removeClass( "d-none" );
+    } else {
+      $('.sameheight').removeClass( "d-none" );
+      $('.itemListElement').addClass( "d-none" );
+      $( "."+ $(this).data("show") ).removeClass("d-none");
+    }
+  })
+  $('.filters .by-day a').on('click', function (event) {
+    event.preventDefault();
+    if ($(this).data("show")=="all") {
+      $('.sameheight').removeClass( "d-none" );
+      $('.itemListElement').removeClass( "d-none" );
+    } else {
+      $('.sameheight').addClass( "d-none" );
+      $('.itemListElement').removeClass( "d-none" );
+      $( "."+ $(this).data("show") ).removeClass("d-none");
+    }
+  })
+  /*   function bodyPadding() {
     $('.body-common').css("padding-top", $('#navbar-top').outerHeight()+"px");
   }
   bodyPadding();
