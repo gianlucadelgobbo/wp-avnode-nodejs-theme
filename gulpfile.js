8a0a6d79fa3254610b27d8d578b73348fe35e30c
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2024-02-27 14:06:06 +0100

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -500,6 +500,7 @@ const compress_js_pac = () => {
 //gulp.task('default', gulp.series(compress_js, compress_js_lpm,css_lpm_bs));
 //gulp.task('default', gulp.series(css_lpm_bs));
 //gulp.task('default', gulp.series(compress_js, compress_js_pac,css_pac_bs));
-gulp.task('default', gulp.series(compress_js, compress_js_electrokids,css_electrokids_bs));
+//gulp.task('default', gulp.series(compress_js, compress_js_electrokids,css_electrokids_bs));
+gulp.task('default', gulp.series(compress_js, compress_js_chromosphere,css_chromosphere_bs));
 //gulp.task('default', gulp.series(compress_js_fotonica,css_fotonica_bs));
 //gulp.task('default', gulp.series(css_lcf_bs, compress_js_lcf));
