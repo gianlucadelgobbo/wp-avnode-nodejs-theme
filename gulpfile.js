e6d0c74d40a771e26a291dbbb15672ee89f58542
Gianluca Del Gobbo <g.delgobbo@flyer.it>
2024-03-30 17:20:12 +0100

    fix

--- gulpfile.js
+++ gulpfile.js
@@ -172,7 +172,7 @@ const compress_js = () => {
 const compress_js_chromosphere = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -196,7 +196,7 @@ const compress_js_chromosphere = () => {
 const compress_js_electrokids = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -220,7 +220,7 @@ const compress_js_electrokids = () => {
 const compress_js_fotonica = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -244,7 +244,7 @@ const compress_js_fotonica = () => {
 const compress_js_flyer = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -268,7 +268,7 @@ const compress_js_flyer = () => {
 const compress_js_lcf = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -292,7 +292,7 @@ const compress_js_lcf = () => {
 const compress_js_linuxclub = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -316,7 +316,7 @@ const compress_js_linuxclub = () => {
 const compress_js_lpm = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -341,7 +341,7 @@ const compress_js_lpm = () => {
 const compress_js_shockart = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -365,7 +365,7 @@ const compress_js_shockart = () => {
 const compress_js_vjtelevision = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -394,7 +394,7 @@ const compress_js_vjtelevision = () => {
 const compress_js_wam = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -418,7 +418,7 @@ const compress_js_wam = () => {
 const compress_js_flxer = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -443,7 +443,7 @@ const compress_js_flxer = () => {
 const compress_js_gianlucadelgobbo = () => {
   return gulp.src([
     config.npmDir + '/jquery/dist/jquery.min.js',
-    config.npmDir + '/popper.js/dist/popper.min.js',
+    config.npmDir + '/popper.js/dist/umd/popper.min.js',
     config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
     './gulp/js/_common/jquery.isotope.min.js',
     './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -467,7 +467,7 @@ const compress_js_gianlucadelgobbo = () => {
 const compress_js_pac = () => {
   return gulp.src([
       config.npmDir + '/jquery/dist/jquery.min.js',
-      config.npmDir + '/popper.js/dist/popper.min.js',
+      config.npmDir + '/popper.js/dist/umd/popper.min.js',
       config.npmDir + '/bootstrap/dist/js/bootstrap.min.js',
       './gulp/js/_common/jquery.isotope.min.js',
       './gulp/js/_common/imagesloaded.pkgd.min.js',
@@ -500,7 +500,7 @@ const compress_js_pac = () => {
 //gulp.task('default', gulp.series(compress_js, compress_js_lpm,css_lpm_bs));
 //gulp.task('default', gulp.series(css_lpm_bs));
 //gulp.task('default', gulp.series(compress_js, compress_js_pac,css_pac_bs));
-gulp.task('default', gulp.series(compress_js, compress_js_electrokids,css_electrokids_bs));
-//gulp.task('default', gulp.series(compress_js, compress_js_chromosphere,css_chromosphere_bs));
+//gulp.task('default', gulp.series(compress_js, compress_js_electrokids,css_electrokids_bs));
+gulp.task('default', gulp.series(compress_js, compress_js_chromosphere,css_chromosphere_bs));
 //gulp.task('default', gulp.series(compress_js_fotonica,css_fotonica_bs));
 //gulp.task('default', gulp.series(css_lcf_bs, compress_js_lcf));
